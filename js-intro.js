60e6301287cee645d04a3c5e7b6fbee8f20b13cf
Ubuntu <ubuntu@ip-172-31-21-165.us-east-2.compute.internal>
2019-10-05 03:05:49 +0000

    completed assessment

--- js-intro.js
+++ js-intro.js
@@ -1,42 +1,154 @@
-// ASSESSMENT 1: INTRO TO JAVASCRIPT
-// Coding practical questions
+// // // ASSESSMENT 1: INTRO TO JAVASCRIPT
+// // // Coding practical questions
 
-// --------------------------- Consider the following variable:
+// // // --------------------------- Consider the following variable:
 
 var mantra = "Be the dev"
 
 // 1a. Write the code that determines if there is a 'B' in mantra.
+// const isB = (string) => {
+//     for (let i = 0; i < string.length; i++) {
+//         let letter = string[i]
+//         if (letter === "B") {
+//             return 'There is a B'
+//         }
+//         else {
+//             return 'There is not a B'
+//         }
+//     }
+// }
+
+// const isB = (string) => {
+//     let stringArray = string.split('')
+//     if (stringArray.includes('B')) {
+//         return 'There is a B'
+//     }
+//     else {
+//         return 'There is not a B'
+//     }
+// }
+
+const isB = (string) => {
+    let stringArray = string.split('')
+    return stringArray.includes('B') ? 'There is a B' : 'There is not a B'
+
+}
+console.log(isB(mantra));
 
 
 
 // 1b. Write the code that determines if there is an 'x' in mantra.
+// const isX = (string) => {
+//     for (let i = 0; i < string.length; i++) {
+//         let letter = string[i]
+//         if (letter === "x") {
+//             return 'There is an x'
+//         }
+//         else {
+//             return 'There is not an x'
+//         }
+//     }
+// }
 
+const isX = (string) => {
+    let stringArray = string.split('')
+    return stringArray.includes('X') ? 'There is an X' : 'There is not an X'
+}
+
+console.log(isX(mantra))
 
 
 // 1c. Write the code that determines if there is a 'v' in mantra.
+
+// const isV = (string) => {
+//     for (let i = 0; i < string.length; i++) {
+//         let letter = string[i]
+//         if (letter === "v") {
+//             return 'There is a v'
+//         }
+//         else {
+//             return 'There is not a v'
+//         }
+//     }
+// }
+
+const isV = (string) => {
+    let stringArray = string.split('')
+    return stringArray.includes('v') ? 'There is a v' : 'There is not a v'
+}
+
+console.log(isV(mantra));
 
 
 
 // 1d. STRETCH: Write a function that can take two arguments - a string and a letter and returns a sentence telling the user if the letter exists within the string.
 
+// const isLetter = (string, searchLetter) => {
+//     string = string.toLowerCase()
+//     for (let i = 0; i < string.length; i++) {
+//         let currentLetter = string[i];
+//         if (currentLetter === searchLetter) {
+//             return `${searchLetter} is present in ${string}.`
+//         }
+//     }
+//     return `${searchLetter} is not present in ${string}.`
+// }
 
 
-// ------------------------------ Consider the following variables:
+const letterExists = (string, letter) => {
+    string = string.toLowerCase()
+    letter = letter.toLowerCase()
+    let stringArray = string.split('')
+    return stringArray.includes(letter) ? `${letter} is present in ${string}` : `${letter} is not present in ${string}`
+
+}
+
+console.log(letterExists(mantra, 'o'));
+
+
+
+// // ------------------------------ Consider the following variables:
 
 var myDog = "Snoopy"
 var myCat = "Garfield"
 
 // 2a. Create a function that returns the longer of the two variables. Expected output: "Garfield"
 
+// const isLonger = (var1, var2) => {
+//     if (var1.length > var2.length) {
+//         return var1
+//     }
+//     else {
+//         return var2
+//     }
+// }
 
+const isLonger = (var1, var2) => {
+    return var1.length > var2.length ? var1 : var2
+}
+
+console.log(isLonger(myDog, myCat))
 
 
 // 2b. Create a function that takes in two arguments and returns an array containing the two variables. Expected output: ["Snoopy", "Garfield"]
 
+// const makeArray = (arg1, arg2) => {
+//     return [arg1, arg2];
+// }
+
+const makeArray = (arg1, arg2) => [arg1, arg2]
+
+console.log(makeArray(myDog, myCat))
 
 
 
 // 2c. STRETCH: Write a function that returns the two variables in all lowercase letters. Expected output: "snoopy garfield"
+
+const makeLowercase = (var1, var2) => {
+    return `${var1.toLowerCase()} ${var2.toLowerCase()}`
+}
+
+console.log(makeLowercase(myDog, myCat))
 
 
 
@@ -48,32 +160,89 @@ var myMessage = "Learning is fun!"
 // 3a. Write the code that logs each letter of the message using a for loop.
 
 
+const logLetters = (message) => {
+    for (let i = 0; i < message.length; i++) {
+        console.log(message[i]);
+    }
+}
+
+logLetters(myMessage);
+
+
+
 
 // 3b. Write the code that logs each letter of the message using map.
+
+const mapLetters = (message) => {
+    let lettersArray = message.split('')
+    lettersArray.map(letter => console.log(letter))
+}
+
+mapLetters(myMessage)
 
 
 
 // 3c. STRETCH: Write the code that logs each letter of the message using a while loop.
 
+const stretchLogLetters = (message) => {
+    let lettersArray = message.split('')
+    while (lettersArray.length > 0) {
+        console.log(lettersArray[0])
+        lettersArray.shift();
+    }
+}
+
+stretchLogLetters(myMessage)
+
 
 
 // 3d. SUPER STRETCH: Write the code that logs each letter of the message using forEach().
 
+const superStretchLogLetters = (message) => {
+    let lettersArray = message.split('')
+    lettersArray.forEach(element => console.log(element))
+}
+
+superStretchLogLetters(myMessage)
 
 
 
-// ---------------------------- Consider the following variable:
 
-var testString = "thisisateststring"
+// // ---------------------------- Consider the following variable:
+
+ var testString = "thisisateststring"
 
 // 4a. Write a function that takes the variable as an argument and returns the string without vowels. Expected output: "thsstststrng"
+
+const noVowels = (string) => {
+    let stringArray = string.split('')
+    let noVowelsArray = stringArray.filter(letter => letter !== 'a').filter(letter => letter !== 'e').filter(letter => letter !== 'i').filter(letter => letter !== 'o').filter(letter => letter !== 'u')
+    return noVowelsArray.join('')
+}
+
+console.log(noVowels(testString))
 
 
 
 
 
 // 4b. STRETCH: Update your function to return an error message if the variable is not a string. Copy and paste your function to add these changes.
+let testArray = ['abcd', 'abcd']
 
+const noVowelsStretch = (variable) => {
+    if (typeof variable !== 'string') {
+        return 'please enter a string'
+    } else {
+        let stringArray = variable.split('');
+        let noVowelsArray = stringArray.filter(letter => letter !== 'a').filter(letter => letter !== 'e').filter(letter => letter !== 'i').filter(letter => letter !== 'o').filter(letter => letter !== 'u')
+        return noVowelsArray.join('');
+        
+    }
+
+}
+
+
+console.log(noVowelsStretch(testArray))
 
 
 
@@ -83,14 +252,43 @@ var testString = "thisisateststring"
 
 var toonimals = [ {name: "Itchy", animal: "mouse"}, {name: "Stimpy", animal: "cat"}, {name: "Daffy", animal: "duck"}, {name: "Scratchy", animal: "cat"}, {name: "Ren", animal: "dog"}, {name: "Felix", animal: "cat"}]
 
-// 5a. Write a function that takes in the toonimals array as an argument and returns an array with only the toon objects that are cats. Expected output: [ { name: "Stimpy", animal: "cat" }, { name: "Scratchy", animal: "cat" }, { name: "Felix", animal: "cat" } ]
+// // 5a. Write a function that takes in the toonimals array as an argument and returns an array with only the toon objects that are cats. Expected output: [ { name: "Stimpy", animal: "cat" }, { name: "Scratchy", animal: "cat" }, { name: "Felix", animal: "cat" } ]
 
+// const findCats = array => {
+//     let catsArray = [];
+//     for (let i = 0; i < array.length; i++) {
+//         let animalObject = array[i]
+//         if (animalObject.animal === 'cat') {
+//             catsArray.push(animalObject);
+//         }
+//     }
+//     return catsArray
+// }
+
+const findCats = array => {
+    return array.filter(animalObject => animalObject.animal === 'cat')
+}
+
+console.log(findCats(toonimals))
 
 
 
 //5b. Write a function that returns only the names of all the non-cats. Expected output: "Itchy" "Daffy" "Ren"
 
 
+const noCats = (array) => {
+    let noCatsArray = array.filter(animalObject => animalObject.animal !== 'cat')
+    return noCatsArray.forEach(element => console.log(element.name))
+}
+
+noCats(toonimals)
+
 
 
 //5c. STRETCH: Create a function that will print a sentence for every toon stating their name and their animal type. Use string interpolation. Expected output: "Itchy is a mouse" "Stimpy is a cat" "Daffy is a duck" "Scratchy is a cat" "Ren is a dog" "Felix is a cat"
+
+const makeSentence = (array) => {
+    array.forEach(animalObject => console.log(`${animalObject.name} is a ${animalObject.animal}`))
+}
+
+makeSentence(toonimals);
--- js-intro.md
+++ js-intro.md
@@ -6,48 +6,70 @@ Answer the following questions. First, without external resources. Challenge you
 1. What is a function? Why would you use one?
 
   Your answer:
-
+  A function is a type of code that will do something when you run it. You would use one when you want to get your code to do something.
+  
   Researched answer:
-
+  A block of code that follows specific instructions when it is called to produce an output. You would use a function as a building block to perform specific tasks in an application.
 
 
 2. List 5 examples of JavaScript array methods.
 
-  Your answer:
+  Your answer: 
+    .pop() - removes the last element from an array
+    .push(element) - adds an element to the end of an array
+    .shift() - removes the first element from an array
+    .unshift(element) - adds an element to the beginning of an array
+    .join(separator) - joins the elements of an array into a string by a separator
 
   Researched answer:
-
-
+    .toString() - converts an array into a string of comma separated values
+    .splice(position where elements added, how many elements to remove, new elements to be added) - add new items to an array at a specific position within the array
+    .concat() - merge existing arrays into a combined array
+    .slice(startingArgument, noninclusive endingArgument) - copies a piece of an array into a new array
+    .sort() - sort an array alphabetically
 
 3. Explain the difference between console.log() and return.
 
-  Your answer:
+  Your answer: console.log() is is useful for debugging because it allows you to see what is going on with your code. Return indicates what is executed by your code, but will not show up on the screen.
 
-  Researched answer:
+  Researched answer: console.log() logs out information to your console and so is useful for degugging. Return will return the value of executing the function and terminate the function.
 
 
 
 4. In regards to functions, what is an argument? What is the difference between an argument and a variable?
 
-  Your answer:
+  Your answer: An argument is something that is passed into a function so that the function can execute using the argument. A variable is a container for a piece of data. An argument is a placeholder in a function. When the function is called, the argument in the function can be a variable or the piece of information itself.
 
-  Researched answer:
-
-
+  Researched answer: An argument is the value passed into the function when it is called. A parameter is a variable that is defined in the function declaration to receive the arguments
 
 5. Give a brief description of proper pair programming techniques. What are the rolls of each person?
 
-  Your answer:
+  Your answer: Proper pair programming techniques would include: 
+    - Having one person be the navigator and one person be the driver. 
+    - Taking turns
+    - Explaining what you are thinking
+    - Listening to your partner and allowing each person to contribute ideas
+    - Use pseudocode to determine the plan for the problem and then go into coding the solution
+    
+    Role of navigator: dictate the code to be written, help with debugging/syntax errors
+    Role of the driver: write the code
 
-  Researched answer:
+  Researched answer: There are many types of pair programming techniques. Some of the techniques include:
+    Driver-navigator: One person is the driver - doing the coding and explaining what they are doing,  while the other person is the navigator - helping the driver stay in the right direction and checking the work of the driver. It is best to switch roles often.
+    
+    Ping-pong: Both people can be driving at any time. A good strategy for this approach is to have one person writing the tests while the other one tries to pass them. It is best to switch roles often.
+    
+    Backseat navigator: The navigator (usually the more senior team member) tells the driver, who has the keyboard, what to do to solve a problem with every little detail. The navigator should provide insights on how to solve the problem but also on why that’s the best solution in mind. 
+    
+    Reference: https://www.businessprocessincubator.com/content/exploring-five-pair-programming-techniques/
 
 
 
 6. Give a brief description of JavaScript as if you were explaining it to someone who doesn't know anything about programming.
 
-  Your answer:
+  Your answer: JavaScript is a programming language that allows webpages and applications to be dynamic and interactive.
 
 
 7. What is one thing we did in class this week you found helpful?  
 
-  Your answer:
+  Your answer: I liked the presentation on pair programming techniques and what to do in certain situations. I did pair programming in a bootcamp prep course and ran into some of those issues, so I think it was nice that you educated us on best practices.
